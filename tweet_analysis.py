a5e3f960908439cf3abad5f2edf94762cd220abe
Eldar Urmanov <urmanov@gmail.com>
2016-03-21 04:03:04 -0400

    commit

--- tweet_analysis.py
+++ tweet_analysis.py
@@ -5,7 +5,11 @@ from pyspark.sql import SQLContext
 from pyspark.sql.functions import col
 from pyspark import SparkContext
 
+
+sc = SparkContext(appName="Tweet Analysis")
+
 sqlContext = SQLContext(sc)
 
 # reading the json-format tweet file for further analysis
-tweet_load = sqlContext.read.json("tweets/cleaned_2012_01_0.json")
+df = sqlContext.read.json("tweets/cleaned_2012_01_0.json")
+
