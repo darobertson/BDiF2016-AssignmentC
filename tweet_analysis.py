507edd8e9d20dd51603ec1541f1d79bc15009662
Eldar Urmanov <urmanov@gmail.com>
2016-03-21 04:06:30 -0400

    commit

--- tweet_analysis.py
+++ tweet_analysis.py
@@ -5,11 +5,19 @@ from pyspark.sql import SQLContext
 from pyspark.sql.functions import col
 from pyspark import SparkContext
 
-
+# setting up SparkContext
 sc = SparkContext(appName="Tweet Analysis")
 
 sqlContext = SQLContext(sc)
 
 # reading the json-format tweet file for further analysis
 df = sqlContext.read.json("tweets/cleaned_2012_01_0.json")
+
+# Temporary Table
+df.registerTempTable("df")
+
+
+# only keep tweets in English
+lang_en_tweet = sqlContext.sql("SELECT * FROM df WHERE lang = 'en'")
+lang_en_tweet.registerTempTable("lang_en_tweet")
 
